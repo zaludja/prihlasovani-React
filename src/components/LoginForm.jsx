5a282510edb0ce853993c9c9c7685bac2be4aad5
zaludja <jakub.zalud.0804@gmail.com>
2025-05-23 10:32:34 +0200

    nefunguje

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import InputField from './InputField';
+import PasswordField from './PasswordField';
+//import Button from './Button';
+import Divider from './Divider';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
+import { Link } from 'react-router-dom';
+
+function LoginForm() {
+  const [formError, setFormError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: validace nebo API volání
+  };
+
+  return (
+    <>
+      {formError && <div className="error-msg">{formError}</div>}
+
+      <form className="login-form" onSubmit={handleSubmit}>
+        <InputField
+          label="Email *"
+          type="email"
+          id="email"
+          placeholder="Zadejte Váš email"
+        />
+
+        <PasswordField/>
+
+        <Link to="/zapomenute-heslo" className="forgot-password">
+          🔒 Zapomenuté heslo
+        </Link>
+
+        <Button type="primary"  className="login-btn">
+           Přihlásit se
+        </Button>
+
+        <Divider text="nebo" />
+
+        <Button type="primary" block size="large" className="create-account-btn">
+          Vytvořit nový účet Moje Amber
+        </Button>
+
+        <p className="help-text">
+          Jste náš stávající dárce a nemůžete se přihlásit?{' '}
+          <a href="#">Klikněte zde</a> a nastavte si heslo.
+        </p>
+      </form>
+    </>
+  );
+}
+
+export default LoginForm;
